41a311421869e4959737880272d2c3fdb53b41df
Lucas Almeida <lucasdealmeida.ss@gmail.com>
2021-07-20 09:56:59 -0300

    Implementando logica de checar localStorage para saber se o usuário esta logado ou não

--- js/login.js
+++ js/login.js
@@ -23,20 +23,131 @@ modalCloseButton.onclick = (e) => {
 "password": "cityslicka"
 */
 
-modalLoginButton.onclick = () => {
+// Fluxo de login
+
+// Função que valida o login (email e senha)
+const validateLogin = (email, password) => {
+  const loginContainer = document.querySelector(".input-login");
+
+  const errorMessage = document.createElement("span");
+  errorMessage.classList.add("login-error-message");
+
+  let error = {
+    errorMessageElement: errorMessage,
+    field: "",
+    parentElement: loginContainer,
+  };
+
+  if (password.trim() === "") {
+    errorMessage.innerHTML = "a senha não pode ser vazia";
+    error = { ...error, field: "password" };
+  }
+
+  if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+    errorMessage.innerHTML = "o email foi digitado incorretamente";
+    error = { ...error, field: "email" };
+  }
+
+  if (email.trim() === "") {
+    errorMessage.innerHTML = "o email não pode ser vazio";
+    error = { ...error, field: "email" };
+  }
+
+  if (errorMessage.innerHTML.trim() !== "") {
+    loginContainer.insertBefore(errorMessage, loginContainer.children[4]);
+    return { ...error, valid: false };
+  }
+
+  return { ...error, valid: true };
+};
+
+// Função que limpa as alterações feitas pela validação
+const revertValidationError = (validation, element) => {
+  const { parentElement, errorMessageElement } = validation;
+
+  element.classList.remove("input-error");
+  parentElement.removeChild(errorMessageElement);
+
+  element.onkeydown = null;
+};
+
+// Função de logout
+const logout = () => {
+  // limpar token do storage
+  localStorage.removeItem("token");
+  loginButton.innerHTML = "área do aluno";
+  loginButton.onclick = () => {
+    modalContainer.style.display = "flex";
+  };
+};
+
+// Função de login
+const login = () => {
+  // Inicializando constantes e variáveis
   var xmlhttp = new XMLHttpRequest();
-  const emailValue = document.querySelector(".login-email").value;
-  const passwordValue = document.querySelector(".login-password").value;
+  const emailElement = document.querySelector(".login-email");
+  const passwordElement = document.querySelector(".login-password");
+  const validation = validateLogin(emailElement.value, passwordElement.value); // Aqui já é feita a validação dos campos
+
+  // Alterando texto do botão para carregando (melhor para UX)
+  modalLoginButton.innerHTML = "Carregando...";
+
+  // Aplicando resultado da validação nos campos
+  if (!validation.valid) {
+    if (validation.field === "email") {
+      emailElement.classList.add("input-error");
+      emailElement.onkeydown = () =>
+        revertValidationError(validation, emailElement);
+    }
+    if (validation.field === "password") {
+      passwordElement.classList.add("input-error");
+      passwordElement.onkeydown = () =>
+        revertValidationError(validation, passwordElement);
+    }
+    modalLoginButton.innerHTML = "Entrar";
+    return null;
+  }
+
+  // Fazendo requisição ao reqres.in
   xmlhttp.open("POST", "https://reqres.in/api/login", true);
   xmlhttp.setRequestHeader("Content-Type", "application/json");
   xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-      console.log("Loggado");
+      // Login bem-sucedido -> Alterando botão de login para botão de logout e salvando token no localStorage
+
       loginButton.innerHTML = "sair";
+      loginButton.onclick = logout;
+
+      emailElement.value = "";
+      passwordElement.value = "";
+
+      localStorage.setItem("token", JSON.parse(xmlhttp.responseText).token);
+
       modalContainer.style.display = "none";
-    } else {
-      console.log(xmlhttp.responseText);
+      modalLoginButton.innerHTML = "Entrar";
+    } else if (xmlhttp.status === 400) {
+      // Login mal-sucedido -> Exibindo mensagem de erro e revertendo botão 'Carregando...' para 'Entrar'
+
+      const { errorMessageElement, parentElement } = validation;
+      errorMessageElement.innerHTML = "e-mail ou senha incorretos";
+      parentElement.insertBefore(
+        errorMessageElement,
+        parentElement.children[4]
+      );
+
+      passwordElement.onkeydown = () =>
+        revertValidationError(validation, passwordElement);
+
+      modalLoginButton.innerHTML = "Entrar";
     }
   };
-  xmlhttp.send(JSON.stringify({ email: emailValue, password: passwordValue }));
+  xmlhttp.send(
+    JSON.stringify({
+      email: emailElement.value,
+      password: passwordElement.value,
+    })
+  );
 };
+
+// Mudando o que acontece ao clicar no botao de login
+modalLoginButton.onclick = login;
