0b4563d1d341a5fb12768822da7f01b386523e3a
Vineasouza <vineasouza33@hotmail.com>
2021-07-19 22:16:49 -0300

    Implementando do Login com a API ReqRes utilizando JS

--- js/login.js
+++ js/login.js
@@ -1,6 +1,9 @@
 const loginButton = document.querySelector(".login-button");
 const modalContainer = document.querySelector(".modal-container");
 const modalCloseButton = document.querySelector(".modal-close-button");
+const modalLoginButton = document.querySelector(".modal-button");
+const emailValue = document.querySelector(".login-email").value;
+const passwordValue = document.querySelector(".login-password").value;
 
 // Quando clicar no botao "Area do Aluno", abre o modal de login
 loginButton.onclick = () => {
@@ -13,4 +16,25 @@ modalContainer.onclick = (e) => {
 };
 modalCloseButton.onclick = (e) => {
   if (e.target === modalCloseButton) modalContainer.style.display = "none";
+};
+
+/*
+"email": "eve.holt@reqres.in",
+"password": "cityslicka"
+*/
+
+modalLoginButton.onclick = () => {
+  var xmlhttp = new XMLHttpRequest();
+  const emailValue = document.querySelector(".login-email").value;
+  const passwordValue = document.querySelector(".login-password").value;
+  xmlhttp.open("POST", "https://reqres.in/api/login", true);
+  xmlhttp.setRequestHeader("Content-Type", "application/json");
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+      console.log("Loggado");
+    } else {
+      console.log(xmlhttp.responseText);
+    }
+  };
+  xmlhttp.send(JSON.stringify({ email: emailValue, password: passwordValue }));
 };
